b4d235afca6b8cee5f863c3ea9edb2f66ee72e2f
hero1s <hero1s@qq.com>
2022-09-02 09:29:59 +0800

    guid改成cpp编译

--- core/plugins/src/guid/lguid.cpp
+++ core/plugins/src/guid/lguid.cpp
@@ -3,28 +3,29 @@
 #include <string.h>
 #include <time.h>
 
+extern "C" {
 #include "lua.h"
 #include "lauxlib.h"
+}
 
-
-//i  - group，10位，(0~1023)
-//g  - index，10位(0~1023)
-//s  - 序号，13位(0~8912)
+//i  - group， 10位(0~1023)
+//g  - index， 10位(0~1023)
+//s  - 序号，  13位(0~8912)
 //ts - 时间戳，30位
 //共63位，防止出现负数
 
-#define GROUP_BITS  10
-#define INDEX_BITS  10
-#define SNUM_BITS   13
-#define TIME_BITS   30
+static constexpr int GROUP_BITS = 10;
+static constexpr int INDEX_BITS = 10;
+static constexpr int SNUM_BITS  = 13;
+static constexpr int TIME_BITS  = 30;
 
 //基准时钟：2021-01-01 08:00:00
-#define BASE_TIME   1625097600
+static constexpr int BASE_TIME = 1625097600;
 
-#define MAX_GROUP   ((1 << GROUP_BITS) - 1) //1024 - 1
-#define MAX_INDEX   ((1 << INDEX_BITS) - 1) //1024 - 1
-#define MAX_SNUM    ((1 << SNUM_BITS) - 1)  //8912 - 1
-#define MAX_TIME    ((1 << TIME_BITS) - 1)
+static constexpr int MAX_GROUP = ((1 << GROUP_BITS) - 1); //1024 - 1
+static constexpr int MAX_INDEX = ((1 << INDEX_BITS) - 1); //1024 - 1
+static constexpr int MAX_SNUM  = ((1 << SNUM_BITS) - 1);  //8912 - 1
+static constexpr int MAX_TIME  = ((1 << TIME_BITS) - 1);
 
 //每一group独享一个id生成种子
 static int serial_inedx_table[(1 << GROUP_BITS)] = { 0 };
@@ -161,8 +162,12 @@ static const luaL_Reg lguid_funcs[] = {
 	{ NULL, NULL },
 };
 
-LUAMOD_API int luaopen_lguid(lua_State* L) {
-	luaL_checkversion(L);
-	luaL_newlib(L, lguid_funcs);
-	return 1;
+extern "C" {
+	LUAMOD_API int luaopen_lguid(lua_State* L) {
+		luaL_checkversion(L);
+		luaL_newlib(L, lguid_funcs);
+		return 1;
+	}
 }
+
+
