4655922b704006f48fccae94446160bc2845853d
hero1s <hero1s@qq.com>
2023-10-25 14:14:51 +0800

    修改clickhouse及测试

--- script/store/clickhouse_mgr.lua
+++ script/store/clickhouse_mgr.lua
@@ -1,12 +1,11 @@
 --clickhouse_mgr.lua
 local log_err       = logger.err
-
+local mrandom       = math_ext.random
 local event_mgr     = hive.get("event_mgr")
+local config_mgr    = hive.get("config_mgr")
 
 local SUCCESS       = hive.enum("KernCode", "SUCCESS")
 local MYSQL_FAILED  = hive.enum("KernCode", "MYSQL_FAILED")
-
-local MAIN_DBID     = environ.number("HIVE_DB_MAIN_ID")
 
 local ClickHouseMgr = singleton()
 local prop          = property(ClickHouseMgr)
@@ -22,22 +21,40 @@ end
 
 --初始化
 function ClickHouseMgr:setup()
-    local MysqlDB = import("driver/mysql.lua")
-    local drivers = environ.driver("HIVE_MYSQL_URLS")
-    for i, conf in ipairs(drivers) do
-        local clickhouse_db    = MysqlDB(conf, i)
-        self.clickhouse_dbs[i] = clickhouse_db
+    local MysqlDB  = import("driver/mysql.lua")
+    local database = config_mgr:init_table("database", "name")
+    for _, conf in database:iterator() do
+        local drivers = environ.driver(conf.url)
+        if drivers and #drivers > 0 then
+            local dconf = drivers[1]
+            if dconf.driver == "mysql" then
+                local mysql_db                 = MysqlDB(dconf)
+                self.clickhouse_dbs[conf.name] = mysql_db
+                if conf.default then
+                    self.default_db = mysql_db
+                end
+            end
+        end
     end
 end
 
 --查找clickhouse db
-function ClickHouseMgr:get_db(db_id)
-    return self.clickhouse_dbs[db_id or MAIN_DBID]
+function ClickHouseMgr:get_db(db_name, hash_key)
+    local mysqldb
+    if not db_name or db_name == "default" then
+        mysqldb = self.default_db
+    else
+        mysqldb = self.clickhouse_dbs[db_name]
+    end
+    if mysqldb and mysqldb:set_executer(hash_key or mrandom()) then
+        return mysqldb
+    end
+    return nil
 end
 
-function ClickHouseMgr:query(db_id, primary_id, sql)
-    local clickhouse_db = self:get_db(db_id)
-    if clickhouse_db and clickhouse_db:set_executer(primary_id) then
+function ClickHouseMgr:query(db_name, primary_id, sql)
+    local clickhouse_db = self:get_db(db_name, primary_id)
+    if clickhouse_db then
         local ok, res_oe = clickhouse_db:query(sql)
         if not ok then
             log_err("[ClickHouseMgr][query] query {} failed, because: {}", sql, res_oe)
@@ -47,9 +64,9 @@ function ClickHouseMgr:query(db_id, primary_id, sql)
     return MYSQL_FAILED, "clickhouse db not exist"
 end
 
-function ClickHouseMgr:execute(db_id, primary_id, stmt, ...)
-    local clickhouse_db = self:get_db(db_id)
-    if clickhouse_db and clickhouse_db:set_executer(primary_id) then
+function ClickHouseMgr:execute(db_name, primary_id, stmt, ...)
+    local clickhouse_db = self:get_db(db_name, primary_id)
+    if clickhouse_db then
         local ok, res_oe = clickhouse_db:execute(stmt, ...)
         if not ok then
             log_err("[ClickHouseMgr][execute] execute {} failed, because: {}", stmt, res_oe)
@@ -59,8 +76,8 @@ function ClickHouseMgr:execute(db_id, primary_id, stmt, ...)
     return MYSQL_FAILED, "clickhouse db not exist"
 end
 
-function ClickHouseMgr:prepare(db_id, sql)
-    local clickhouse_db = self:get_db(db_id)
+function ClickHouseMgr:prepare(db_name, primary_id, sql)
+    local clickhouse_db = self:get_db(db_name, primary_id)
     if clickhouse_db and clickhouse_db:set_executer() then
         local ok, res_oe = clickhouse_db:prepare(sql)
         if not ok then
