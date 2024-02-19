5882079832d1d493065c0ac577bc9a8d1f96f926
hero1s <hero1s@qq.com>
2024-02-19 16:59:56 +0800

    修改配置解析

--- script/store/mysql_mgr.lua
+++ script/store/mysql_mgr.lua
@@ -25,9 +25,8 @@ function MysqlMgr:setup()
     local MysqlDB  = import("driver/mysql.lua")
     local database = config_mgr:init_table("database", "name")
     for _, conf in database:iterator() do
-        local drivers = environ.driver(conf.url)
-        if drivers and #drivers > 0 then
-            local dconf = drivers[1]
+        local dconf = environ.driver(conf.url)
+        if dconf then
             if dconf.driver == "mysql" then
                 local mysql_db            = MysqlDB(dconf)
                 self.mysql_dbs[conf.name] = mysql_db
