1b1ce65e1454edfe8250aa9628d91da1ece0dff0
DSKY\toney.yin <toney.yin@idreamsky.com>
2024-03-22 15:20:52 +0800

    lmdb添加close接口

--- server/cache/lmdb_mgr.lua
+++ server/cache/lmdb_mgr.lua
@@ -1,14 +1,23 @@
-local LMDB      = import("driver/lmdb.lua")
-local log_debug = logger.debug
-local sformat   = string.format
+local LMDB       = import("driver/lmdb.lua")
+local log_debug  = logger.debug
+local sformat    = string.format
 
-local LmdbMgr   = singleton()
-local prop      = property(LmdbMgr)
+local update_mgr = hive.get("update_mgr")
+
+local LmdbMgr    = singleton()
+local prop       = property(LmdbMgr)
 prop:reader("open_status", false)
 prop:reader("db", nil)
 
 function LmdbMgr:__init()
     self:setup()
+    update_mgr:attach_quit(self)
+end
+
+function LmdbMgr:on_quit()
+    if self.db then
+        self.db:close()
+    end
 end
 
 function LmdbMgr:setup()
