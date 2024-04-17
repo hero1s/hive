2dda79dd3994a62bfe1b4c3c9c7b1f1c6d914e1b
hero1s <hero1s@qq.com>
2024-04-17 15:35:27 +0800

    修改自动查找vs路径

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+
 @echo off
+SETLOCAL EnableDelayedExpansion
 
 if exist .\bin\lib (
     del .\bin\lib\*.dll /f /s /q
@@ -8,9 +10,21 @@ if exist .\bin\lib (
     md .\bin\lib
 )
 
-::C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE 环境变量
-devenv .\hive.sln /Rebuild
-devenv .\hive.sln /Build
+if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
+  echo "WARNING: You need VS 2019 version or later (for vswhere.exe)"
+)
+
+set vswherestr=^"!ProgramFiles(x86)!\Microsoft Visual Studio\Installer\vswhere.exe^" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+for /f "usebackq tokens=*" %%i in (`!vswherestr!`) do (  
+  set BUILDVCTOOLS=%%i\Common7\IDE
+  echo BUILDVCTOOLS: !BUILDVCTOOLS!
+  if not exist !BUILDVCTOOLS!\devenv.com (
+    echo Error: Cannot find VS2019 or later
+    exit /b 2
+  )
+"!BUILDVCTOOLS!\devenv.com" .\hive.sln /Rebuild
+"!BUILDVCTOOLS!\devenv.com" .\hive.sln /Build
+)
 
 move .\bin\*.dll .\bin\lib
 
