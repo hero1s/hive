c18a8d5217cb5b1b7326120435332f86263ee64f
hero1s <hero1s@qq.com>
2024-08-24 21:49:56 +0800

    删除敏感信息

--- server/config/service_cfg.lua
+++ server/config/service_cfg.lua
@@ -224,62 +224,12 @@ return {
         pre_services = {"lobby"}, --[[ 安全退出前置服务 ]]
     },
     {
-        id = 205, --[[ 服务id ]]
-        name = 'tlog_client', --[[ 服务名字 ]]
-        mode = 3, --[[ 模式 ]]
-        rely_router = false, --[[ 依赖路由 ]]
-        hash = 0, --[[ 服务固定hash ]]
-        sole_ip = false, --[[ 校验ip ]]
-        safe_stop = true, --[[ 安全退出 ]]
-        pre_services = {}, --[[ 安全退出前置服务 ]]
-    },
-    {
         id = 206, --[[ 服务id ]]
         name = 'ai', --[[ 服务名字 ]]
         mode = 1, --[[ 模式 ]]
         rely_router = true, --[[ 依赖路由 ]]
         hash = 0, --[[ 服务固定hash ]]
         sole_ip = false, --[[ 校验ip ]]
-        safe_stop = true, --[[ 安全退出 ]]
-        pre_services = {}, --[[ 安全退出前置服务 ]]
-    },
-    {
-        id = 207, --[[ 服务id ]]
-        name = 'tencent_sdk', --[[ 服务名字 ]]
-        mode = 1, --[[ 模式 ]]
-        rely_router = true, --[[ 依赖路由 ]]
-        hash = 0, --[[ 服务固定hash ]]
-        sole_ip = false, --[[ 校验ip ]]
-        safe_stop = true, --[[ 安全退出 ]]
-        pre_services = {"lobby"}, --[[ 安全退出前置服务 ]]
-    },
-    {
-        id = 208, --[[ 服务id ]]
-        name = 'cdkey', --[[ 服务名字 ]]
-        mode = 1, --[[ 模式 ]]
-        rely_router = true, --[[ 依赖路由 ]]
-        hash = 0, --[[ 服务固定hash ]]
-        sole_ip = false, --[[ 校验ip ]]
-        safe_stop = true, --[[ 安全退出 ]]
-        pre_services = {}, --[[ 安全退出前置服务 ]]
-    },
-    {
-        id = 209, --[[ 服务id ]]
-        name = 'pay', --[[ 服务名字 ]]
-        mode = 1, --[[ 模式 ]]
-        rely_router = true, --[[ 依赖路由 ]]
-        hash = 0, --[[ 服务固定hash ]]
-        sole_ip = false, --[[ 校验ip ]]
-        safe_stop = true, --[[ 安全退出 ]]
-        pre_services = {}, --[[ 安全退出前置服务 ]]
-    },
-    {
-        id = 210, --[[ 服务id ]]
-        name = 'login_key', --[[ 服务名字 ]]
-        mode = 1, --[[ 模式 ]]
-        rely_router = true, --[[ 依赖路由 ]]
-        hash = 0, --[[ 服务固定hash ]]
-        sole_ip = true, --[[ 校验ip ]]
         safe_stop = true, --[[ 安全退出 ]]
         pre_services = {}, --[[ 安全退出前置服务 ]]
     },
